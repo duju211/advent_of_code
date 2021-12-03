57bc07a392039652d8b366fb5604f5a57c6afbd2
OaCantona <duringjulian@gmail.com>
2021-12-03 19:23:24 +0100

    Solution for 2nd part

--- 2021/03_12/03_b.R
+++ 2021/03_12/03_b.R
@@ -3,9 +3,9 @@ library(tidyverse)
 input <- c(
   "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100",
   "10000", "11001", "00010", "01010")
+input <- read_lines("2021/03_12/input.txt")
 input_length <- unique(str_length(input))
 col_names <- str_glue("bit_{1:input_length}")
-
 
 df_bits <- tibble(txt = input) |>
   separate(
@@ -14,7 +14,6 @@ df_bits <- tibble(txt = input) |>
 
 df_bits_oxygen <- df_bits
 df_bits_co2 <- df_bits
-
 
 erg_vec_oxygen <- vector(mode = "list", length = input_length)
 for (i in seq_len(input_length)) {
@@ -27,6 +26,21 @@ for (i in seq_len(input_length)) {
 }
 ind_sol <- min(which(map_int(erg_vec_oxygen, nrow) == 1))         
 erg_vec_oxygen[[ind_sol]] |>
+  pivot_longer(cols = everything()) |>
+  mutate(name = input_length - as.integer(str_extract(name, "\\d+"))) |>
+  summarise(erg = sum(value * 2^name))
+
+erg_vec_co2 <- vector(mode = "list", length = input_length)
+for (i in seq_len(input_length)) {
+  if (sum(df_bits_co2[, i]) < nrow(df_bits_co2) / 2)
+    most_freq <- 1
+  else
+    most_freq <- 0
+  erg_vec_co2[[i]] <- df_bits_co2[df_bits_co2[, i] == most_freq, ]
+  df_bits_co2 <- df_bits_co2[df_bits_co2[, i] == most_freq,]
+}
+ind_sol <- min(which(map_int(erg_vec_co2, nrow) == 1))         
+erg_vec_co2[[ind_sol]] |>
   pivot_longer(cols = everything()) |>
   mutate(name = input_length - as.integer(str_extract(name, "\\d+"))) |>
   summarise(erg = sum(value * 2^name))
