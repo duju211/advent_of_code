042b7118036b3f17e928d75188a000697bc3ecd2
During <julian.during@trumpf.com>
2022-12-02 11:01:12 +0100

    Simplify

--- 2022/02_12/b.R
+++ 2022/02_12/b.R
@@ -1,34 +1,22 @@
 library(tidyverse)
 
-input <- read_lines("2022/02_12/input_ex.txt")
-
-df_trans <- tibble(
-  abc = c("A", "B", "C"),
-  xyz = c("X", "Y", "Z"))
+input <- read_lines("2022/02_12/input.txt")
 
 df_reward <- tibble(
   abc = c("A", "B", "C"),
   reward = c(1, 2, 3))
 
-df_win <- tibble(
+df_result <- tibble(
   abc = c("A", "B", "C"),
-  win = c("B", "C", "A"))
-
-df_draw <- tibble(
-  abc = c("A", "B", "C"),
-  draw = c("A", "B", "C"))
-
-df_lose <- tibble(
-  abc = c("A", "B", "C"),
+  win = c("B", "C", "A"),
+  draw = c("A", "B", "C"),
   lose = c("C", "A", "B"))
 
 df_games_raw <- tibble(input = input) |>
   separate(input, c("elv", "me"))
 
 df_games <- df_games_raw |>
-  left_join(df_win, by = c("elv" = "abc")) |>
-  left_join(df_draw, by = c("elv" = "abc")) |>
-  left_join(df_lose, by = c("elv" = "abc")) |>
+  left_join(df_result, by = c("elv" = "abc")) |>
   mutate(
     move = case_when(
       me == "X" ~ lose,
@@ -37,11 +25,10 @@ df_games <- df_games_raw |>
   left_join(df_reward, by = c("move" = "abc")) |>
   mutate(
     result = case_when(
-      elv == move ~ 3,
-      elv == "A" & move == "B" ~ 6,
-      elv == "B" & move == "C" ~ 6,
-      elv == "C" & move == "A" ~ 6,
-      TRUE ~ 0),
+      move == win ~ 6L,
+      move == draw ~ 3L,
+      move == lose ~ 0L,
+      TRUE ~ NA_integer_),
     points = reward + result)
 
 df_games |>
